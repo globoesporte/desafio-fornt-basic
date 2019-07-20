7eb38dcf4285877f96370e2e18aa6f5fdfd64197
guhenrique007 <drgustav1997@gmail.com>
2019-07-19 21:01:47 -0300

    Logica Terminada,algum dia partir pro CSS

---
+++ scripts/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+    console.log('%c Arquivo Json', 'color: orange;')
+    console.log({data});
+
+    //buscar atleta
+    $('input.barra-atleta').change(function() {
+        console.log('testando change');
+        $("table.lista-jogadores tbody").empty();
+        data.players.forEach(function (value, i) {
+            let nomeJogador = data.players[i].name;
+            let textoDigitado = $('input.barra-atleta').val();
+            if(nomeJogador.includes(textoDigitado)){
+                value = nomeJogador;
+                console.log('%d: %s', i, value);
+                $('table.lista-jogadores').append('<tr> <td>'+ nomeJogador +'</td> '+
+                '<td>' + data.players[i].date + '</td>'+
+                '<td>' + data.players[i].team.sigla + '</td>'+
+                '<td>' + data.players[i].games + '</td>'+
+                '<td>' + data.players[i].goals + '</td>'+
+                '<td> 00% </td>'+
+                '</tr>');
+            }      
+        })
+        
+    })
+
+    //sorter
+    var table = $('table.lista-jogadores');
+    $('th.nasc_header').add('th.games_header').add('th.goals_header').click(function(){
+        var table = $(this).parents('table.lista-jogadores').eq(0);
+        var ths = table.find('tr:gt(0)').toArray().sort(compare($(this).index())); //pega tudo abaixo do th selecionado
+        this.asc = !this.asc;
+        if(!this.asc)
+           ths = ths.reverse();
+           //$(this).append(' V ');
+
+        for(var i = 0; i < ths.length; i++)
+           table.append(ths[i]);
+
+    });
+
+});
+
+function compare(idx) {
+    return function(a, b) {
+       var A = tableCell(a, idx), B = tableCell(b, idx)
+       return $.isNumeric(A) && $.isNumeric(B) ? 
+          A - B : A.toString().localeCompare(B)
+    }
+}
+
+function tableCell(tr, index){ 
+    return $(tr).children('td').eq(index).text() 
+}
